0fa5cd37a1f01c90ab43947a75be8b188250a632
charlesLoder <charles.w.loder@gmail.com>
2021-08-29 14:03:37 -0400

    Update error handling

--- functions/transliteration.ts
+++ functions/transliteration.ts
@@ -16,24 +16,17 @@ const constructReq = (req: Req) => {
 const getResp = async (req: Req): Promise<Resp> => {
   const method = req.package.toLowerCase();
   const newReq = constructReq(req);
-  try {
-    const resp = await fetch(`${URL}/.netlify/functions/${method}`, newReq);
-
-    if (resp.status !== 200) {
-      // const error = await resp.json();
-      throw resp;
-    }
-    return await resp.json();
-  } catch (error) {
-    throw error;
-  }
+  const resp = await fetch(`${URL}/.netlify/functions/${method}`, newReq);
+  const json = await resp.json();
+  return resp.ok ? json : Promise.reject(json);
 };
 
 const handler: Handler = async (event: HandlerEvent, context) => {
   try {
-    // Only allow POST
     if (event.httpMethod !== "POST") {
-      throw { statusCode: 405, message: "Method Not Allowed" };
+      throw await Promise.reject({
+        message: `${event.httpMethod} Not Allowed`,
+      });
     }
 
     const req: Req = JSON.parse(event.body);
@@ -49,16 +42,23 @@ const handler: Handler = async (event: HandlerEvent, context) => {
     };
   } catch (error) {
     console.log(error);
-
     return {
-      statusCode: error.status || 400,
+      statusCode: 400,
       headers: {
         "access-control-allow-origin": "*",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(await error.json()),
+      body: JSON.stringify(error),
     };
   }
 };
 
 export { handler };
+
+//[Symbol(Response internals)]: {
+//   url: 'https://adoring-swartz-a2dd0c.netlify.app/.netlify/functions/umschrift',
+//   status: 404,
+//   statusText: 'Not Found',
+//   headers: Headers { [Symbol(map)]: [Object: null prototype] },
+//   counter: 0
+// }
